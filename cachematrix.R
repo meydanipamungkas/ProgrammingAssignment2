b17f857bb1b1ccc86c8ac0b488be3d496aa3c744
Meidani Pamungkas <68596953+meydanipamungkas@users.noreply.github.com>
2021-11-25 15:35:01 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,28 @@
-X
+##Here is the code for Programming Assignment that use function
+makeCacheMatrix<-function(x = matrix()){
+  j<-NULL
+  set<-function(y){
+    x<<-y
+    j<<-NULL
+  }
+  get<-function()x
+  setInverse<-function(inverse) j<<-inverse
+  getInverse<-function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+##Same here, changed "mean" to "solve" and "m" to "j"
+cacheSolve<-function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j<-x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat<-x$get()
+  j<-solve(mat,...)
+  x$setInverse(j)
+  j
+}
